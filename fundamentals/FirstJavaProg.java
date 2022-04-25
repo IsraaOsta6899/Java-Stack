b093e6d3ec69f4b158e826c4f3cd2dc3c53e3f23
israa <israa-osta@hotmail.com>
2022-04-25 12:15:11 +0300

    upload first assi

---
+++ fundamentals/FirstJavaProg.java
@@ -0,0 +1,13 @@
+package fundamentals;
+
+public class FirstJavaProg{
+    public static void main(String []args) {
+        System.out.println("My name is Coding Dojo");
+        System.out.println("I am 100 years old");
+        System.out.println("My hometown is Burbank, CA");
+
+        
+
+
+    }
+}
